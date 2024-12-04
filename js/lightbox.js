f98226e335b066c4cccfd1f3cfdfa1a9da75b698
Jaume <jaume.esquerdo@alumnos.cei.es>
2024-12-04 21:38:51 +0100

    optimizando

--- js/lightbox.js
+++ js/lightbox.js
@@ -7,6 +7,14 @@ const lightbox = document.querySelector(`.Lightbox`)
 const grande = document.querySelector('.Grande')
 const closeBtn = document.querySelector('.Close')
 
+//asignar variables a las funciones para guardarlas y poder reutilizarlas
+const closeBtnHandler = () => lightbox.classList.remove('isActive')
+const imgListHandler = (i) => {
+    lightbox.classList.add('isActive')
+    grande.src = imgList[i].src
+
+}
+
 //cuando hago click en .img
     //.lightbox se le ADD la clase isActive
     //.grande se le añade el atributo src de la imagen
@@ -15,16 +23,13 @@ const closeBtn = document.querySelector('.Close')
     //recorre cada img
     imgList.forEach((eachImg,i)=>{
         //se le añade el evento click a cada i(cada img)
-        imgList[i].addEventListener('click', ()=>{
+        imgList[i].addEventListener('pointerdown', ()=>{
 
-            lightbox.classList.add('isActive')
-            grande.src = imgList[i].src
+            imgListHandler(i);
         })
     })
 
     // cuando hago click en close
         // a la clase lightbox se le quita la clase isActive
     
-    closeBtn.addEventListener('click', ()=>{
-        lightbox.classList.remove('isActive')
-    })
+    closeBtn.addEventListener('pointerdown', closeBtnHandler)//poner pointerdown en vez de click para que funcione con el dedo y cond lapices el click
